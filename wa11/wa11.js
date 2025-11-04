bc1e9460795130a160c862341885973647c58fa9
bred1526 <bred1526@colorado.edu>
2025-11-04 15:52:51 -0700

    update

--- wa11/wa11.js
+++ wa11/wa11.js
@@ -2,30 +2,16 @@ let endpoint = "https://newsapi.org/v2/everything?q=pop-culture&pageSize=100&sor
 
 const newsBtn = document.querySelector(".get-news").addEventListener('click', getNews)
 
+const display = document.querySelector(".display");
 // const musicSelect = document.querySelector("").addEventListener('click', setEndpointMusic)
 
 
 const TOPIC_KEYWORDS = {
-  music: ["music","album","song","single","artist","band","tour","concert","playlist","Spotify","Apple Music"],
+  music: ["music","album","song","single","band","tour","concert","playlist","Spotify","Apple Music"],
   movies: ["movie","film","cinema","box office","director","actor","actress","trailer","premiere","Oscars"],
   gaming: ["game","gaming","PlayStation","Xbox","Nintendo","Steam","esports","DLC","RPG","FPS"],
   celebrity: ["celebrity","star","influencer","paparazzi","tabloid","scandal","gossip","viral"]
 };
-
-function filter(){
-for(i=0; i<TOPIC_KEYWORDS.music.length; i++){
-    console.log(TOPIC_KEYWORDS.music[i]);
-    console.log(TOPIC_KEYWORDS.movies[i]);
-    console.log(TOPIC_KEYWORDS.gaming[i]);
-    console.log(TOPIC_KEYWORDS.celebrity[i]);
-
-
-    
-}
-}
-
-
-
 
 async function getNews(){
     console.log("getNews triggered");
@@ -41,10 +27,139 @@ async function getNews(){
         console.log(json);
 
 
-        filter();        
+        filter(json);        
     }
     catch (err) {
         console.log(err)
         alert('Failed to get news');
     }
+}
+
+
+function filter(json){
+
+const topic = document.querySelector('input[name="topic"]:checked');
+
+console.log("filter function = " + topic.value);
+
+console.log("filter json test: " + json);
+
+    let found = false;
+    let counter = 0;
+    let position = 0;
+
+    do{
+        let combined = (json.articles[counter].description + " " + json.articles[counter].title).toLocaleLowerCase();
+        console.log(combined);
+
+        //topics filter
+        if(topic.value == "music"){//Music Filter
+            console.log("topic is music");
+
+            for(i=0; i<10; i++)
+            {
+                console.log("Key word for loop: " + i)
+                if(combined.includes(TOPIC_KEYWORDS.music[i].toLowerCase()))
+                {
+                    found=true;
+                    console.log("article found with keyword: " + TOPIC_KEYWORDS.music[i]);
+                }
+            }
+            
+            if(found==true){
+                position = counter;
+                console.log("position= " + position);
+            }
+        }
+        else if(topic.value == "movies"){//Movie Filter
+            console.log("topic is movies");
+
+            for(i=0; i<10; i++)
+            {
+                console.log("Key word for loop: " + i)
+                if(combined.includes(TOPIC_KEYWORDS.movies[i].toLowerCase()))
+                {
+                    found=true;
+                    console.log("article found with keyword: " + TOPIC_KEYWORDS.movies[i]);
+                }
+            }
+            
+            if(found==true){
+                position = counter;
+                console.log("position= " + position);
+            }
+        }
+        else if(topic.value == "gaming"){//Gaming Filter
+            console.log("topic is gaming");
+
+            for(i=0; i<10; i++)
+            {
+                console.log("Key word for loop: " + i)
+                if(combined.includes(TOPIC_KEYWORDS.gaming[i].toLowerCase()))
+                {
+                    found=true;
+                    console.log("article found with keyword: " + TOPIC_KEYWORDS.gaming[i]);
+                }
+            }
+            
+            if(found==true){
+                position = counter;
+                console.log("position= " + position);
+            }
+        }
+        else if(topic.value == "celebrity"){//Celebrity Filter
+            console.log("topic is celebrity");
+
+            for(i=0; i<8; i++)
+            {
+                console.log("Key word for loop: " + i)
+                if(combined.includes(TOPIC_KEYWORDS.celebrity[i].toLowerCase()))
+                {
+                    found=true;
+                    console.log("article found with keyword: " + TOPIC_KEYWORDS.celebrity[i]);
+                }
+            }
+            
+            if(found==true){
+                position = counter;
+                console.log("position= " + position);
+            }
+        }
+
+        counter++;        
+    }while(found == false && counter < 100)
+
+    displayText(json.articles[position]);
+
+
+// for(i=0; i<TOPIC_KEYWORDS.music.length; i++){
+//     console.log(TOPIC_KEYWORDS.music[i]);
+//     console.log(TOPIC_KEYWORDS.movies[i]);
+//     console.log(TOPIC_KEYWORDS.gaming[i]);
+//     console.log(TOPIC_KEYWORDS.celebrity[i]);
+// }
+}
+
+
+function displayText(json){
+    display.innerHTML = `
+    <h2>Title</h2>
+    <h3>${json.title}</h3>
+
+    <br>
+
+    <h2>Description</h2>
+    <p>${json.description}</p>
+
+    <br>
+
+    <h2>Link</h2>
+    <a href=${json.url}>Web Link</a>
+
+    <br>
+    <br>
+
+    <h2>Content</h2>
+    <p>${json.content}</p>
+    `;
 }
